46a6ddeb5b2a9c74555aa505ec12d639409e33c5
islamov-ildar <39472576+islamov-ildar@users.noreply.github.com>
2025-09-10 16:05:26 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='app-header'>
+            <img
+                src="https://static.vecteezy.com/system/resources/thumbnails/024/553/534/small_2x/lion-head-logo-mascot-wildlife-animal-illustration-generative-ai-png.png"
+                alt="logo" height='100px'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Main = () => {
+    return (
+        <main className='app-content'>
+            <img
+                src="https://img.freepik.com/free-vector/abstract-banner-with-low-poly-plexus-network-communications-design_1048-12914.jpg?semt=ais_hybrid&w=740&q=80"
+                alt="img" width='100%'/>
+            <div>
+                ava+description
+            </div>
+            <div>MyPosts</div>
+            <div>NewPost</div>
+            <div>Post_1</div>
+            <div>Post_2</div>
+        </main>
+    )
+}

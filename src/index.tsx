eebf1042a16130a23fb04c72850b81f1b2e25b4c
islamov-ildar <islamov.ildar@gmail.com>
2025-09-27 12:26:57 +0300

    in progress

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {addNewPost, state, subscribe, updateNewPostText} from "./redux/state";
-
+// import {addNewPost, state, subscribe, updateNewPostText} from "./redux/state";
+import store from './redux/state'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-export const rerenderEntireTree = (state: any, addNewPost: any, updateNewPostText: any) => {
+export const rerenderEntireTree = (store: any) => {
     root.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addNewPost} updateNewPostText={updateNewPostText}/>
+                <App state={store.getState()} addPost={store.addNewPost} updateNewPostText={store.updateNewPostText}/>
             </BrowserRouter>
         </React.StrictMode>
     );
 }
 
-rerenderEntireTree(state, addNewPost, updateNewPostText);
+rerenderEntireTree(store);
 
-subscribe(rerenderEntireTree)
+// subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 reportWebVitals();
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,58 +1,54 @@
+export const store = {
+    _state: {
+        postPage: {
+            newPostText: 'it-kamasutra',
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 3},
+                {id: 2, message: 'It\'s my first post', likesCount: 6},
+                {id: 3, message: 'Yo', likesCount: 12},
+                {id: 4, message: 'KukarekuKukarekuKukareku123', likesCount: 51},
+            ],
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Sveta'},
+                {id: 3, name: 'Kolya'},
+                {id: 4, name: 'Kukareku'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'KukarekuKukarekuKukareku999'},
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    addNewPost() {
+        const newPost = {
+            id: this._state.postPage.posts.length + 1,
+            message: this._state.postPage.newPostText,
+            likesCount: 0
+        }
+        this._state.postPage.newPostText = '';
+        this._state.postPage.posts.push(newPost);
+
+        console.log('addNewPost', this._state);
+        rerenderEntireTree(this._state, this.addNewPost, this.updateNewPostText);
+    },
+    updateNewPostText(newText: string) {
+        this._state.postPage.newPostText = newText
+        console.log(this._state.postPage.newPostText)
+        rerenderEntireTree(this._state, this.addNewPost, this.updateNewPostText);
+    },
+    subscribe(observer: any) {
+        rerenderEntireTree = observer;
+    }
+};
 
 let rerenderEntireTree: any;
 
-const postsData = [
-    {id: 1, message: 'Hi, how are you?', likesCount: 3},
-    {id: 2, message: 'It\'s my first post', likesCount: 6},
-    {id: 3, message: 'Yo', likesCount: 12},
-    {id: 4, message: 'KukarekuKukarekuKukareku123', likesCount: 51},
-]
-
-const dialogs = [
-    {id: 1, name: 'Dima'},
-    {id: 2, name: 'Sveta'},
-    {id: 3, name: 'Kolya'},
-    {id: 4, name: 'Kukareku'},
-]
-
-const messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'KukarekuKukarekuKukareku999'},
-]
-
-export const state = {
-    postPage: {
-        posts: postsData,
-        newPostText: 'it-kamasutra'
-    },
-    dialogsPage: {
-        dialogs: dialogs,
-        messages: messages,
-    },
-}
-
-export const addNewPost = () => {
-    const newPost = {
-        id: state.postPage.posts.length + 1,
-        message: state.postPage.newPostText,
-        likesCount: 0
-    }
-    state.postPage.newPostText = '';
-    state.postPage.posts.push(newPost);
-
-    console.log('addNewPost', state);
-    rerenderEntireTree(state, addNewPost, updateNewPostText);
-
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.postPage.newPostText = newText
-    console.log(state.postPage.newPostText)
-    rerenderEntireTree(state, addNewPost, updateNewPostText);
-}
-
-export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer;
-}
+export default store

0902b8b5280194f80163be2b7683a4106fca3a99
islamov-ildar <islamov.ildar@gmail.com>
2025-10-01 22:54:05 +0300

    Lesson - 50(only theory), 51

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import s from './users.module.css'
+import axios from "axios";
+import userPhoto from "../../assets/userPlug.png";
 
 export const Users = (props: any) => {
     console.log('Users', props);
 
     if(props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photoUrl: 'https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/1fbc51ed-5043-5b3f-9fd9-8bdc23ed67a1/e02a087b-3868-5efb-b449-370ff253b769.jpg', followed: true, fullName: 'Dart Vader', status: 'Luke, I am your father', location: {city: 'Nabu Coda', country: 'Alderaan'}},
-            {id: 2, photoUrl: 'https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/1fbc51ed-5043-5b3f-9fd9-8bdc23ed67a1/e02a087b-3868-5efb-b449-370ff253b769.jpg', followed: false, fullName: 'Mace Windu', status: 'I have dismantled and destroyed over 100,000 of you Type One battle droids.', location: {city: 'Javin', country: 'Four'}},
-            {id: 3, photoUrl: 'https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/1fbc51ed-5043-5b3f-9fd9-8bdc23ed67a1/e02a087b-3868-5efb-b449-370ff253b769.jpg', followed: true, fullName: 'Obi Van', status: 'Only a Sith deals in absolutes.', location: {city: 'Coruscant', country: 'City'}},
-            {id: 4, photoUrl: 'https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/1fbc51ed-5043-5b3f-9fd9-8bdc23ed67a1/e02a087b-3868-5efb-b449-370ff253b769.jpg', followed: false, fullName: 'Luke Skywalker', status: 'Do or do not. There is no try.', location: {city: 'Stewjon', country: 'Town'}},
-        ]);
+
+        const baseUrl = 'https://social-network.samuraijs.com/api/1.0/users'
+
+        axios.get(baseUrl).then((res: any) => {
+            console.log('axios.get users', res.data.items);
+            props.setUsers(res.data.items)
+        })
+        // props.setUsers([
+        //     {id: 1, photoUrl: '', followed: true, fullName: 'Dart Vader', status: 'Luke, I am your father', location: {city: 'Nabu Coda', country: 'Alderaan'}},
+        //     {id: 2, photoUrl: '', followed: false, fullName: 'Mace Windu', status: 'I have dismantled and destroyed over 100,000 of you Type One battle droids.', location: {city: 'Javin', country: 'Four'}},
+        //     {id: 3, photoUrl: '', followed: true, fullName: 'Obi Van', status: 'Only a Sith deals in absolutes.', location: {city: 'Coruscant', country: 'City'}},
+        //     {id: 4, photoUrl: '', followed: false, fullName: 'Luke Skywalker', status: 'Do or do not. There is no try.', location: {city: 'Stewjon', country: 'Town'}},
+        // ]);
     }
 
     return <div>
-        {props.users.map((u: any) => <div key={u.id+u.fullName}>
+        {props.users.map((u: any) => <div key={u.id}>
         <span>
-            <div><img src={u.photoUrl} alt="avatar" className={s.userImg}/></div>
+            <div><img src={u.photos.small ? u.photos.small : userPhoto} alt="avatar" className={s.userImg}/></div>
             <div>{ u.followed ?
                 <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
                 <button onClick={() => props.follow(u.id)}>Follow</button>}
@@ -24,15 +33,14 @@ export const Users = (props: any) => {
         </span>
         <span>
             <span>
-                <div>{u.fullName}</div>
-                <div>{u.status}</div>
+                <div>{u.name}</div>
+                <div>{u.id}</div>
             </span>
             <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                {/*<div>{u.location.country}</div>*/}
+                {/*<div>{u.location.city}</div>*/}
             </span>
         </span>
-        {u.name}
     </div>)}
     </div>
 }

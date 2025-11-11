0f530a950f565947b6ea9ae11dd6c779403b3cae
islamov-ildar <islamov.ildar@gmail.com>
2025-11-11 23:14:09 +0300

    Lesson - 89 in progress

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('render without crashing', () => {
+    const div = document.createElement('div');
+    const root = createRoot(div);
+    root.render(<App />);
+    root.unmount();
+})
